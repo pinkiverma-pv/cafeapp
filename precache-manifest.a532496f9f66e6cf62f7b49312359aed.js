97b4ec5ce754bd92c24ed571792552ae91d388d7
Pinki <pinki.verma17.pv@gmail.com>
2020-09-05 06:24:32 +0530

    Updates

---
+++ precache-manifest.a532496f9f66e6cf62f7b49312359aed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5e51f9dd7d74aaea252a69d563831f",
+    "url": "/cafeapp/index.html"
+  },
+  {
+    "revision": "f233b0886803e488e208",
+    "url": "/cafeapp/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "2dcda41b13ff789aba31",
+    "url": "/cafeapp/static/css/main.20d7cce3.chunk.css"
+  },
+  {
+    "revision": "f233b0886803e488e208",
+    "url": "/cafeapp/static/js/2.c8c3d2ea.chunk.js"
+  },
+  {
+    "revision": "24b8054ddc1445462862104fe5fc876e",
+    "url": "/cafeapp/static/js/2.c8c3d2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dcda41b13ff789aba31",
+    "url": "/cafeapp/static/js/main.720c521c.chunk.js"
+  },
+  {
+    "revision": "93c5151d13ff198b2b0a",
+    "url": "/cafeapp/static/js/runtime-main.3592dc43.js"
+  }
+]);
